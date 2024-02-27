1900432443332a6ce6b43d018a559cc8e66bd340
taufiqqq <muhdtaufiqbinjurimi@gmail.com>
2024-02-28 00:23:11 +0800

    Update inboxPage.dart
    created dummy inbox, and allow deleting, inbox number update

--- lib/pages/inbox/inboxPage.dart
+++ lib/pages/inbox/inboxPage.dart
@@ -2,14 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:scoutify/components/components.dart';
 import 'package:scoutify/pages/inbox/inboxdetail.dart';
 
+import '../../model/inbox.dart';
+
 class InboxMainPage extends StatefulWidget {
   const InboxMainPage({super.key});
-  
+
   @override
   State<InboxMainPage> createState() => _InboxMainPageState();
 }
 
 class _InboxMainPageState extends State<InboxMainPage> {
+  List<Inbox> dummyInboxes = [
+    Inbox(
+      inboxId: '1',
+      name: 'Welcome to Scoutify',
+      category: 'Welcome',
+      details: 'Hey there, Scout!',
+      imageurl: 'welcome_image_url',
+      isRead: false,
+    ),
+    Inbox(
+      inboxId: '2',
+      name: 'New Activity Has Recorded',
+      category: 'Activity',
+      details: 'Congratulations',
+      imageurl: 'activity_image_url',
+      isRead: false,
+    ),
+    Inbox(
+      inboxId: '3',
+      name: 'Update Your Johor Scout Digital ID',
+      category: 'Update',
+      details: 'Hey there, it\'s time to update your Johor Scout Digital ID',
+      imageurl: 'update_image_url',
+      isRead: false,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,12 +53,12 @@ class _InboxMainPageState extends State<InboxMainPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 25,
-                  backgroundColor: Color(0xFFF44236),
+                  backgroundColor: const Color(0xFFF44236),
                   child: Text(
-                    '3',
-                    style: TextStyle(
+                    dummyInboxes.length.toString(),
+                    style: const TextStyle(
                         fontFamily: 'Poppins',
                         color: Colors.white,
                         fontSize: 20),
@@ -68,13 +97,31 @@ class _InboxMainPageState extends State<InboxMainPage> {
               height: 20,
             ),
             Flexible(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: 5,
-                  //Hardcoded itemCount
-                  itemBuilder: (context, index) {
-                    return buildInboxMessage(index);
-                  }),
+              child: dummyInboxes.isEmpty
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/inbox_empty.png',
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          // Replace with your image asset path
+                        ),
+                        const SizedBox(height: 5),
+                        const Text('Your inbox is empty'),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    )
+                  : ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: dummyInboxes.length,
+                      itemBuilder: (context, index) {
+                        return buildInboxMessage(
+                            dummyInboxes.elementAt(index), index);
+                      },
+                    ),
             )
           ],
         ),
@@ -82,118 +129,132 @@ class _InboxMainPageState extends State<InboxMainPage> {
     );
   }
 
-  Widget buildInboxMessage(int index) {
+  Widget buildInboxMessage(Inbox inbox, int index) {
     return Dismissible(
-      
-        background: Padding(
-          padding: const EdgeInsets.only(bottom : 10.0),
-          child: Container(
-            color: Color.fromARGB(255, 255, 0, 0),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 20.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.delete,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                    Text(
-                      'Delete',
-                      style: TextStyle(
-                          fontFamily: 'Poppins',
-                          color: Colors.white,
-                          fontSize: 14),
-                    )
-                  ],
-                ),
+      background: Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: Container(
+          color: const Color.fromARGB(255, 255, 0, 0),
+          child: const Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                  Text(
+                    'Delete',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                        fontSize: 14),
+                  )
+                ],
               ),
             ),
           ),
         ),
-        key: Key(index.toString()),
-        direction: DismissDirection.endToStart,
-        dismissThresholds: const {DismissDirection.endToStart: 0.3},
-        onDismissed: (direction) {
-          print(direction.name);
-        },
-        child: Padding(
-          padding: const EdgeInsets.only(bottom : 10.0),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => InboxDetailPage()));
-              },
-              child: Ink(
-                color: Colors.white,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const CircleAvatar(
-                      radius: 26,
-                      backgroundColor: Color(0xFF2E3B78),
-                      child: Icon(
-                        Icons.person_outline,
-                        color: Colors.white,
-                        size: 34,
-                      ),
+      ),
+      key: UniqueKey(),
+      direction: DismissDirection.endToStart,
+      dismissThresholds: const {DismissDirection.endToStart: 0.3},
+      onDismissed: (direction) {
+        //ask confirmation to delete, i think better for ux
+        print(direction.name);
+        setState(
+          () {
+            dummyInboxes.removeAt(index);
+          },
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 10.0),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => const InboxDetailPage()));
+            },
+            child: Ink(
+              color: Colors.white,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 26,
+                    backgroundColor: Color(0xFF2E3B78),
+                    child: Icon(
+                      Icons.person_outline,
+                      color: Colors.white,
+                      size: 34,
                     ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          const Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Column(
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    'Welcome to Scoutify',
-                                    style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        fontSize: 14,
-                                        fontWeight: FontWeight.w500),
+                                    inbox.name,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w500,
+                                    ),
                                   ),
                                   Text(
-                                    'Hey there, Scout!',
-                                    style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        color: Color(0xFF9397A0),
-                                        fontWeight: FontWeight.w500),
-                                  )
+                                    inbox.details,
+                                    textAlign: TextAlign.start,
+                                    overflow: TextOverflow.ellipsis,
+                                    maxLines: 1,
+                                    style: const TextStyle(
+                                      fontFamily: 'Poppins',
+                                      color: Color(0xFF9397A0),
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
                                 ],
                               ),
-                              Icon(
-                                Icons.arrow_forward_ios,
-                                color: Color(0xFF2E3B78),
-                              )
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            height: 1,
-                            width: MediaQuery.sizeOf(context).width,
-                            color: Colors.black.withOpacity(0.25),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                            ),
+                            const Icon(
+                              Icons.arrow_forward_ios,
+                              color: Color(0xFF2E3B78),
+                            )
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          height: 1,
+                          width: MediaQuery.sizeOf(context).width,
+                          color: Colors.black.withOpacity(0.25),
+                        )
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ),
         ),
+      ),
     );
   }
 
